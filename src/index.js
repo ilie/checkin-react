d264367c586a50bb0002e64adf0ea62bca77814c
Ilie Florea <floreailie87@gmail.com>
2022-06-14 11:45:07 +0200

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./store/auth-context";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
@@ -21,7 +20,3 @@ root.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
